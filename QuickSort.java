42b4b43e7f3031330bfe6df401ddeda37baa7461
YS_workPC <ong_ys86@yahoo.com.sg>
2023-05-08 11:22:21 +0800

    correct the code to account for more case

--- QuickSort.java
+++ QuickSort.java
@@ -1,8 +1,9 @@
+package javaDP;
 import java.util.Arrays;
 
 public class QuickSort {
   private static void quickSort(int[] a, int start, int end){
-    if (start==end) return ;
+    if (start>=end) return ;
     int lp = start;
     int rp = end;
     int pivot = a[end];
@@ -12,11 +13,15 @@ public class QuickSort {
       while( a[rp]>=pivot && lp <rp) rp--;
       swap(a, lp, rp);
     }
-    if (lp == rp){
-      if(a[lp] > pivot ) swap(a, lp, end);
-      quickSort(a,start,lp-1);
-      quickSort(a,lp+1, end);
-    }
+    if (a[lp] > a[end]) swap(a, lp, end);
+    else lp = end;
+    quickSort(a,start,lp-1);
+    quickSort(a,lp+1, end);
+  //   if (lp == rp){
+  //     if(a[lp] > pivot ) swap(a, lp, end);
+  //     quickSort(a,start,lp-1);
+  //     quickSort(a,lp+1, end);
+  //   }
   }
 
   private static void swap(int[] a, int p1, int p2 ){
@@ -27,7 +32,7 @@ public class QuickSort {
   }
 
   public static void main(String[] args) {
-    int[] a = {51,51,52,6,2,3,-20,9,7,8};
+    int[] a = {51,52,6,2,3,3,3,5,9,7,8,0,0,-1};
     quickSort(a, 0, a.length-1);
     System.out.println(Arrays.toString(a));
   }
